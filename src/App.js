78ca9abe88345e9eb2b1e097002146288020c324
Jonathan <jonathanldominguez@gmail.com>
2018-04-20 09:57:55 -0500

    common implementation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Header } from './components/common';
 
 const App = () => (
   <View>
+    <Header headerText="Authentication" />
     <Text>React Auth</Text>
   </View>
 );
