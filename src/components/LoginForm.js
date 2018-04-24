8baa8a235b1158b476bc33084a8fa20401a210d3
Jonathan <jonathanldominguez@gmail.com>
2018-04-24 16:03:24 -0500

    Spinner and Login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,19 +1,60 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
-import { Button, Card, CardSection, Input } from './common';
+import { Text } from 'react-native';
+import { Button, Card, CardSection, Input, Spinner } from './common';
 
+const styles = {
+  errorStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red',
+  },
+};
 
 class LoginForm extends Component {
   constructor(props) {
     super(props);
     this.onButtonPress = this.onButtonPress.bind(this);
+    this.onLoginSuccess = this.onLoginSuccess.bind(this);
+    this.onLoginFail = this.onLoginFail.bind(this);
   }
-    state = { email: '', password: '' }
+    state = {
+      email: '', password: '', error: '', loading: false,
+    }
     onButtonPress() {
-      console.log(`email ${this.state.email}`);
-      firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password);
+      const { email, password } = this.state;
+      console.log(email, password);
+      this.setState({ error: '', loading: true });
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then(this.onLoginSuccess, this.onLoginFail)
+        .catch(() => {
+          firebase
+            .auth()
+            .createUserWithEmailAndPassword(email, password)
+            .then(this.onLoginSuccess, this.onLoginFail);
+        });
+    }
+    onLoginSuccess() {
+      this.setState({
+        email: '', password: '', loading: false, error: '',
+      });
+    }
+    onLoginFail(error) {
+      this.setState({ error: error.message, loading: false });
+    }
+    renderButton() {
+      if (this.state.loading) {
+        return <Spinner size="small" />;
+      }
+      return (
+        <Button onPress={this.onButtonPress}>
+          Log In
+        </Button>);
     }
     render() {
+      const { errorStyle } = styles;
       return (
         <Card>
           <CardSection>
@@ -33,8 +74,10 @@ class LoginForm extends Component {
               secureTextEntry
             />
           </CardSection>
+          <Text style={errorStyle}>{this.state.error}</Text>
+          <CardSection />
           <CardSection>
-            <Button onPress={this.onButtonPress}>Log In</Button>
+            {this.renderButton()}
           </CardSection>
         </Card>
       );
